1f759151d359d64d0ee762d53ac68e97fe39bf30
Akunna Onyekachi <akunna1mail@gmail.com>
2026-05-01 16:26:03 -0400

    updated layout.tsx and clientLayout.tsx

--- app/clientLayout.tsx
+++ app/clientLayout.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import Image from "next/image";
 import Menu from "./components/menu";
 import Submenu from "./components/submenu";
 import Footer1 from "./components/footer1";
 import Footer2 from "./components/footer2";
+
+// Contains actual UI structure (e.g., page layout, footer, floating buttons, menu, etc.) => clientLayout.tsx 
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -18,6 +21,18 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
 
       <Footer1 />
       <Footer2 />
+
+      {/* Floating Coffee button */}
+        <a
+          href="https://buymeacoffee.com/akunna"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="fixed bottom-4 right-4 bg-[#D8BFD8] p-3 rounded-full shadow-lg hover:scale-110 active:scale-110 transition-transform duration-200 flex items-center justify-center"
+          aria-label="Buy me a coffee button"
+        >
+          <Image src="/photos/coffee.svg" alt="Buy me a coffee image" width={35} height={35} />
+        </a>
+
     </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import { Roboto } from "next/font/google";
 import ClientLayout from "./clientLayout";
 
+// Global setup + Fonts + Metadata + HTML/body structure => Layout.tsx 
+
 // Roboto font with the weights
 const roboto = Roboto({
   subsets: ["latin"],
@@ -11,7 +13,7 @@ const roboto = Roboto({
 
 export const metadata: Metadata = {
   title: "The Counterfeit",
-  description: "Created by Akunna Onyekachi",
+  description: "A Christian eschatology web app studying end-times prophecy and the rise of its greatest counterfeit form during the last days.",
 };
 
 export default function RootLayout({
