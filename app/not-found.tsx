96d83d8e0db87769ab0104b45364ebad2a5ea1db
Akunna Onyekachi <akunna1mail@gmail.com>
2026-05-02 15:48:59 -0400

    Updated global.css to include backgroundImage instead

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,15 +6,7 @@ import { FaRegNewspaper, FaExclamationTriangle } from "react-icons/fa";
 export default function NotFound() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-white px-6 text-center relative overflow-hidden">
-
-      {/* Newspaper-style subtle background */}
-      <div
-        className="absolute inset-0 opacity-[0.06]"
-        style={{
-          backgroundImage:
-            "repeating-linear-gradient(0deg, #000 0px, #000 1px, transparent 1px, transparent 6px)",
-        }}
-      />
+      <div className="absolute inset-0 opacity-[0.06] backgroundImage"/>
 
       {/* Content layer */}
       <div className="relative z-10 flex flex-col items-center">
