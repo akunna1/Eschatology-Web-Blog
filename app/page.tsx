bc90f2addc114319d8dece69b96da4fac777f95f
Akunna Onyekachi <akunna1mail@gmail.com>
2026-08-10 16:59:01 -0400

    Removed redundant comments

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,12 @@ import Verse from "@/components/verse";
 import Button from "@/components/button";
 import Resources from "@/components/resources";
 
-// client components can be imported into server components, but not the other way around
 export default function Home () {
   return (
     <div>
       < Homepic />
       < Verse />
       < Button />
-      < Resources /> {/* serverside component that fetches data and passes it to a client component */}
+      < Resources />
     </div>
 )};
