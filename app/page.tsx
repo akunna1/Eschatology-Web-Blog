1d8b445bf019dc76325c2eb0be63adde9c690559
Akunna Onyekachi <akunna1mail@gmail.com>
2026-08-10 17:15:05 -0400

    Add 60-second page revalidation for Contentful updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Homepic from "@/components/homepic";
 import Verse from "@/components/verse";
 import Button from "@/components/button";
-import Resources from "@/components/resources";
+import Resources from "@/components/resources"; // server component with client component inside
+
+export const revalidate = 60;
 
 export default function Home () {
   return (
